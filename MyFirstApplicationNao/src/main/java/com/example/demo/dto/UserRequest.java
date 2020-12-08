a448774f811cc27049875d5e0c09005732d93c77
原田尚輝 <a148106.nao@gmail.com>
2020-12-08 13:56:04 +0900

    アドレス正規表現追加

--- MyFirstApplicationNao/src/main/java/com/example/demo/dto/UserRequest.java
+++ MyFirstApplicationNao/src/main/java/com/example/demo/dto/UserRequest.java
@@ -16,6 +16,8 @@ public class UserRequest implements Serializable{
 	@NotEmpty(message="名前を入力してください")
 	private String name;
 	
+	@Pattern(regexp="^([a-zA-Z0-9])+([a-zA-Z0-9\\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\\._-]+)+$",
+			message="メールアドレスの形式で入力してください")
 	private String email;
 	
 	@Pattern(regexp = "^\\d{2,4}-\\d{2,4}-\\d{4}$", message = "電話番号の形式で入力してください")
